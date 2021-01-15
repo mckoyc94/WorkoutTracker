760bcdf5bbe075f48a0b0f73bc45ff67a24fe08a
Cmac <mckoyc@gmail.com>
2021-01-15 16:52:26 -0500

    added new js files to models folder and linked server.js with api and html routes

--- Develop/server.js
+++ Develop/server.js
@@ -9,7 +9,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+require("./routes/api-routes")(app)
+require("./routes/html-routes")(app)
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
