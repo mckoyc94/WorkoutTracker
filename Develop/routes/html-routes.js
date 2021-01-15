aa9d521bc6c3792d5187fae471b0d4323bae138a
Cmac <mckoyc@gmail.com>
2021-01-15 17:12:07 -0500

    added get routes for app to server.js and added semantic language to server.js

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose")
+const path = require("path")
 
 module.exports = app => {
-    
+    app.get("/stats", (req, res)=> {
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
+    })
 }
--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,34 @@
+// Calls necessary components for backend
 const express = require("express");
 const mongoose = require("mongoose");
 
+//Defines PORT
 const PORT = process.env.PORT || 5000;
 
+// Defines use of express
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// Links app to MongoDB database 'workout'
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+// Links app with api routes
 require("./routes/api-routes")(app)
-require("./routes/html-routes")(app)
 
+// Sends User to Index page
+app.get("/", (req, res)=> {
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+//Sends User to Stats page
+app.get("/stats", (req, res)=> {
+  res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
+// Tells app to listen on PORT
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
