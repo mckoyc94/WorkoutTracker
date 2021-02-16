b325bb757e118fc606f4d1953b0f85a3c2053ede
Cmac <mckoyc@gmail.com>
2021-02-16 12:55:53 -0500

    fixed html routes and seeder

--- Develop/models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    workout : require("/exercise.js")
-}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Calls necessary components for backend
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path")
 
 //Defines PORT
 const PORT = process.env.PORT || 5000;
@@ -19,14 +20,19 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 require("./routes/api-routes")(app)
 
 // Sends User to Index page
-// app.get("/", (req, res)=> {
-//   res.sendFile(path.join(__dirname, "../public/index.html"))
-// })
+app.get("/", (req, res)=> {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+})
 
-// //Sends User to Stats page
-// app.get("/stats", (req, res)=> {
-//   res.sendFile(path.join(__dirname, "../public/stats.html"))
-// })
+//Sends User to Stats page
+app.get("/stats", (req, res)=> {
+  res.sendFile(path.join(__dirname, "./public/stats.html"))
+})
+
+//Sends User to Exercise page
+app.get("/exercise", (req, res)=> {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"))
+})
 
 // Tells app to listen on PORT
 app.listen(PORT, () => {
