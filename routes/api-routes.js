c4fc9d051c36057f254ddebfa5288e3ed5dd0648
Cmac <mckoyc@gmail.com>
2021-02-16 14:10:37 -0500

    added a put method to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -48,5 +48,14 @@ module.exports = app => {
         })
    })
 
+   app.put("/api/workouts/:id", (req, res) => {
+       Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
+        .then(exercise => {
+            res.json(exercise)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+   })
 
 }
