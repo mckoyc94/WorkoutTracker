4a38804249e9b888c9474efc3553fe5501462bc8
Cmac <mckoyc@gmail.com>
2021-02-16 13:18:43 -0500

    added Get method to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose')
 const db = require("../models")
+const exercise = require('../models/exercise')
+const Workout = db.Workout
 
 module.exports = app => {
-    
+   app.get("/api/workout", (req, res) => {
+       Workout.find({})
+        .sort({name: -1})
+        .then(exercise => {
+            res.json(exercise)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+   })
 }
